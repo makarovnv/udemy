74082c89d6fcd1283e209d6b06f53bd6421eb658
tproger <tproger@example.com>
2023-07-18 11:35:47 +0500

    task_3

---
+++ course udemy/task_3/task_3.py
@@ -0,0 +1,17 @@
+model_car = 'model 3'
+color_car = 'red'
+car_year = 2020
+number_of_door = 5
+color_car = 'black'
+print(model_car)
+print(color_car)
+print(car_year)
+
+human_age_1 = 2
+human_age_2 = 284
+human_age_3 = 26
+human_age_4 = 68
+human_age_5 = 25
+number_of_human = 5
+age_avr = (human_age_1 + human_age_4 + human_age_2 + human_age_3 + human_age_5) / number_of_human
+print(age_avr)
