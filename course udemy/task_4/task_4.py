b54eba9464077594e76d2bf3355b124f929f7804
tproger <tproger@example.com>
2023-07-19 21:05:50 +0500

    task_4 olala новая ветка?

---
+++ course udemy/task_4/task_4.py
@@ -0,0 +1,18 @@
+my_first_name = 'Nikolai'
+my_last_name = 'Makarov'
+my_age = 32
+print ('Hi! My name is ' + my_first_name + ' ' + my_last_name + ', I\'m ' + str(my_age) + ' years old.')
+
+print('\t\tBaa, baa, black sheep,\n\t\tHave you any wool?' + """
+\t\tYes sir, yes sir,
+\t\tThree bags full
+
+\t\tOne for the master,
+\t\tOne for the dame,
+\t\tAnd one for the little boy
+        Who lives down the lane
+
+\t\tBaa, baa, black sheep,
+\t\tHave you any wool?
+\t\tYes sir, yes sir,
+\t\tThree bags full""")
