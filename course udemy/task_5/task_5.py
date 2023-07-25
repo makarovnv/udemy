0f80395c88b9355ba921746ceb72bccb410682fe
tproger <tproger@example.com>
2023-07-25 11:27:04 +0500

    task_4 olala новая ветка?

---
+++ course udemy/task_5/task_5.py
@@ -0,0 +1,11 @@
+text = 'Hello Python!'
+print(text[3])
+
+print('Hello Python!'[3])
+
+text = 'Hello Python!'
+print(text[0:2])
+print('Hello Python!'[0:2])
+
+text = 'Hello Python!'
+print(text[6] + 'a' + text[8:10])
