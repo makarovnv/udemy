a5dc61877585b3ad73de273975b9907d97a0d1d5
tproger <tproger@example.com>
2023-08-06 22:01:19 +0500

    13

---
+++ course udemy/task_13.py
@@ -0,0 +1,11 @@
+x = int(input('Enter any number: '))
+if x == 7:
+print('7 is a lucky number! Today is your lucky day!')
+else:
+print('Thank you! Have a nice day!')
+
+x = int(input('Enter an integer number: '))
+if x % 2 == 0:
+print('The number is even')
+else:
+print('The number is odd')
